c3dd30ffbbadd5f21bc08128f67656cea9c9b379
Blackie360 <felixkent360@gmail.com>
2023-12-15 19:19:56 +0300

    update

--- app/(dashboard)/(routes)/upload/_components/UploadForm.jsx
+++ app/(dashboard)/(routes)/upload/_components/UploadForm.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const UploadForm = () => {
   return (
-    <div>
+    <div className='text-center'>
       
+<div class="flex items-center justify-center w-full">
+    <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-black border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 outline-dashed outline-2 outline-offset-2">
+        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+            <svg class="w-12 h-12 mb-4 text-purple-500 dark:text-purple-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+            </svg>
+            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+            <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+        </div>
+        <input id="dropzone-file" type="file" class="hidden" />
+    </label>
+</div> 
+<button disabled className='p-2 bg-purple-500 text-white w-[30%] rounded-full mt-5 disabled:bg-gray-500'>Upload</button>
+
     </div>
   )
 }
--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -3,9 +3,9 @@ import UploadForm from './_components/UploadForm'
 
 const upload = () => {
   return (
-    <div>
-      <h2>
-        <strong>Start Uploading file  and share </strong>
+    <div className='p-5 px-8 md:px-8'>
+      <h2 className='text-[20px] text-center m-5'>
+        <strong >Start <span className='text-purple-700'>Uploading</span> file  and <span className='text-purple-700'>share</span> </strong>
       </h2>
      <UploadForm />
     </div>
