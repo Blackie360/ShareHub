fadd68130c04c08888d7c758899744dcb4e80367
Blackie360 <felixkent360@gmail.com>
2023-12-18 13:56:42 +0300

    update

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -3,15 +3,16 @@ import React, { useEffect, useState } from 'react';
 import UploadForm from './_components/UploadForm';
 import { app } from '@/firebaseconfig';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
-import {doc, getFirestore, setDoc} from 'firebase/firestore';
+import { doc, getFirestore, setDoc } from 'firebase/firestore';
 import { useUser } from "@clerk/nextjs";
 import generateRandomString from '@/app/Actions/GenerateRandom';
 import { useRouter } from 'next/navigation';
+
 const Upload = () => {
-  const router=useRouter();
-  const {user}=useUser();
+  const router = useRouter();
+  const { user } = useUser();
   const db = getFirestore(app);
-  const[fileDocId, setFileDocId]=useState();
+  const [fileDocId, setFileDocId] = useState('');
   const [uploadCompleted, setUploadCompleted] = useState(false);
   const [progress, setProgress] = useState();
   const storage = getStorage(app);
@@ -31,14 +32,15 @@ const Upload = () => {
           // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
           const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
           console.log('Upload is ' + progress + '% done');
-          setProgress(progress),
+          setProgress(progress);
 
           // Upload completed successfully, now we can get the download URL
-    progress==100 && getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log('File available at', downloadURL);
-      saveInfo(file, downloadURL);
-    });
-
+          if (uploadTask.snapshot.state === "success") {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              console.log('File available at', downloadURL);
+              saveInfo(file, downloadURL);
+            });
+          }
         },
         (error) => {
           console.error('Error during upload:', error.message);
@@ -53,29 +55,48 @@ const Upload = () => {
       console.error('Error uploading file:', error.message);
     }
   };
-  const saveInfo=async(file, downloadURL)=>{
-    const docId=Date.now().toString();
-    await setDoc(doc(db, "uploadedFile", docId),{
-      fileName:file?.name,
-      fileSize:file?.size,
-      fileType:file?.type,
-      fileUrl:downloadURL,
-      userEmail:user?.primaryEmailAddress.emailAddress,
-      userName:user?.fullName,
-      password:'',
-      id:docId,
-      shortUrl:process.env.NEXT_PUBLIC_BASE_URL+generateRandomString()
 
+  // Random string 
+  const randomString = () => {
+    const length = 10; // You can adjust the length as needed
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+
+    return result;
+  };
+
+  // Save info  
+  const saveInfo = async (file, downloadURL) => {
+    const docId = randomString();
+    await setDoc(doc(db, "uploadedFile", docId), {
+      fileName: file?.name,
+      fileSize: file?.size,
+      fileType: file?.type,
+      fileUrl: downloadURL,
+      userEmail: user?.primaryEmailAddress.emailAddress,
+      userName: user?.fullName,
+      password: '',
+      id: docId,
+      shortUrl: process.env.NEXT_PUBLIC_BASE_URL + generateRandomString()
     });
     setFileDocId(docId);
-  }
-useEffect(()=>{
-  uploadCompleted&&
-  setTimeout(()=>{
-    setUploadCompleted(false);
-    router.push('/preview/'+fileDocId );
-  },2000)
-}, [uploadCompleted==true]);
+    setUploadCompleted(true);
+  };
+
+  useEffect(() => {
+    if (fileDocId) {
+      console.log("Redirecting...");
+      setTimeout(() => {
+        console.log("Trying to redirect now");
+        router.push('/preview/' + fileDocId);
+      }, 500);
+    }
+  }, [fileDocId]);
 
   return (
     <div className="p-5 px-8 md:px-8">
@@ -85,8 +106,7 @@ useEffect(()=>{
           <span className="text-purple-700">share</span>
         </strong>
       </h2>
-      <UploadForm uploadBtnClick={(file) => uploadFile(file)}
-      progress={progress} />
+      <UploadForm uploadBtnClick={(file) => uploadFile(file)} progress={progress} />
     </div>
   );
 };
