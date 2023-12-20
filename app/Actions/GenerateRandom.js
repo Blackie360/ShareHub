975cb5f03f355033fd7cde50baa0e4316fa8f9fd
Blackie360 <felixkent360@gmail.com>
2023-12-20 21:39:23 +0300

    update

--- app/Actions/GenerateRandom.js
+++ app/Actions/GenerateRandom.js
@@ -1,6 +1,6 @@
 function generateRandomString(length) {
   let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghIjklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
