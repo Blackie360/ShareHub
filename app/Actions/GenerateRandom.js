eb289873a19c8b5615881952453afb9a85657d48
Blackie360 <felixkent360@gmail.com>
2023-12-22 21:34:35 +0300

    update

--- app/Actions/GenerateRandom.js
+++ app/Actions/GenerateRandom.js
@@ -1,10 +1,36 @@
-function generateRandomString(length) {
+// Utilize crypto for better randomness  
+const crypto = require('crypto');
+
+// Cache previously generated strings
+const previousStrings = []; 
+
+function generateRandomString(length, characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') {
+
+  // Input validation
+  if(length < 1) throw new Error('Length must be > 0');
+
   let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghIjklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  const charsLength = characters.length;
+
+  // Loop length times
+  for(let i = 0; i < length; i++) {
+
+    // Get random character in chars string
+    result += characters[crypto.randomInt(0, charsLength - 1)];
+
   }
+
+  // Ensure uniqueness  
+  while(previousStrings.includes(result)) {
+    result = generateRandomString(length, characters); 
+  }
+
+  // Store generated string  
+  previousStrings.push(result);
+
   return result;
+
 }
-export default generateRandomString;
+
+// Export the function
+module.exports = generateRandomString;
