1c029be7b272ea574bc58e3eedaed766b4432171
Blackie360 <felixkent360@gmail.com>
2024-01-04 15:48:18 +0300

    update

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -6,9 +6,8 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { doc, getFirestore, setDoc } from 'firebase/firestore';
 import { useUser } from "@clerk/nextjs";
 import generateRandomString from '@/app/Actions/GenerateRandom';
-import { useRouter } from 'next/navigation';
+
 const Upload = () => {
-  const router = useRouter();
   const { user } = useUser();
   const db = getFirestore(app);
   const [fileDocId, setFileDocId] = useState('');
@@ -28,12 +27,10 @@ const Upload = () => {
       uploadTask.on(
         'state_changed',
         (snapshot) => {
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
           const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
           console.log('Upload is ' + progress + '% done');
           setProgress(progress);
 
-          // Upload completed successfully, now we can get the download URL
           if (uploadTask.snapshot.state === "success") {
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
               console.log('File available at', downloadURL);
@@ -45,9 +42,7 @@ const Upload = () => {
           console.error('Error during upload:', error.message);
         },
         () => {
-          // Upload completed successfully
           console.log('File uploaded successfully');
-          // You can add any additional logic here if needed
         }
       );
     } catch (error) {
@@ -55,7 +50,6 @@ const Upload = () => {
     }
   };
 
-  // Random string 
   const randomString = () => {
     const length = 10; 
     const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
@@ -87,17 +81,21 @@ const Upload = () => {
   };
 
   useEffect(() => {
-    if (uploadCompleted) {
-      console.log("Redirecting...");
-      const redirectTimer = setTimeout(() => {
-        console.log("Trying to redirect now");
-        router.push('/preview/' + fileDocId);
-      }, 2000);
+    const redirectUser = async () => {
+      if (uploadCompleted) {
+        console.log("Redirecting...");
+        try {
+          // Use window.location.href for a client-side redirect
+          window.location.href = '/preview/' + fileDocId;
+          console.log("Redirected successfully");
+        } catch (error) {
+          console.error("Error redirecting:", error.message);
+        }
+      }
+    };
 
-      // Clear the timer if the component unmounts before the timeout
-      return () => clearTimeout(redirectTimer);
-    }
-  }, [uploadCompleted, fileDocId, router]);
+    redirectUser();
+  }, [uploadCompleted, fileDocId]);
 
   return (
     <div className="p-5 px-8 md:px-8">
