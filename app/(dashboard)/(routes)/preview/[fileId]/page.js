592138a19b7711a6739ea543f937652d4c8627f5
Blackie360 <felixkent360@gmail.com>
2024-01-08 10:23:31 +0300

    preview

--- app/(dashboard)/(routes)/preview/[fileId]/page.js
+++ app/(dashboard)/(routes)/preview/[fileId]/page.js
@@ -1,11 +1,15 @@
 "use client";
+import { app } from '@/firebaseconfig';
+import { getFirestore } from 'firebase/firestore';
 import React, { useEffect } from 'react'
 
 const FilePreview = ({params}) => {
+  const db = getFirestoreFirestore(appp);
     useEffect(()=>{
 
         console.log(params?.fileId)
     },[])
+    const 
   return (
     <div>
       preview
