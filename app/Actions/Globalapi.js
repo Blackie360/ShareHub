38d66e9bf921a20c017da9924e2f3d5ec509ce07
Blackie360 <felixkent360@gmail.com>
2024-01-12 21:23:42 +0300

    resend

---
+++ app/Actions/Globalapi.js
@@ -0,0 +1,7 @@
+const { data } = require('autoprefixer');
+const {default: axios} = require('axios');
+
+const SendEmail=(data) => axios.post('/api/send', data);
+
+
+export default {SendEmail}
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-    publicRoutes: ['/'],
+    publicRoutes: ['/', '/api/send'], // Add '/api/send' to the publicRoutes array
 });
- 
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
- 
