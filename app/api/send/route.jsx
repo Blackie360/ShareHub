921eb12ffe6a68f060925a147461b220244ed4b5
Blackie360 <felixkent360@gmail.com>
2024-01-12 20:40:15 +0300

    resend

--- app/api/send/route.jsx
+++ app/api/send/route.jsx
@@ -1,4 +1,5 @@
-import { EmailTemplate } from '../../../components/EmailTemplate';
+
+import { EmailTemplate } from '../../_component/email-template.jsx';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -6,8 +7,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
   try {
     const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
+      from: 'shareit@resend.dev',
+      to: ['felixkent360@gmail.com'],
       subject: 'Hello world',
       react: EmailTemplate({ firstName: 'John' }),
     });
