495915fb8993bc58af55626baa815d5b62e7616b
Blackie360 <felixkent360@gmail.com>
2024-01-18 09:42:38 +0300

    updates

--- app/api/send/route.jsx
+++ app/api/send/route.jsx
@@ -1,21 +1,23 @@
 import { EmailTemplate } from '../../_component/email-template.jsx';
 import { Resend } from 'resend';
+import { NextResponse } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
+  const responce=await req.json();
   try {
-    const { firstName, file } = await req.json();
+    
 
     const data = await resend.emails.send({
       from: 'shareit@resend.dev',
       to: ['felixkent360@gmail.com'],
       subject: 'Hello world',
-      react: <EmailTemplate firstName={firstName} file={file} />,
+      react: EmailTemplate({responce}),
     });
 
-    return Response.json(data);
+    return NextResponse.json(data);
   } catch (error) {
-    return Response.json({ error });
+    return NextResponse.json({ error });
   }
 }
