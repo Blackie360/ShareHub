e464c84fdcff2c762e87ea70d034f5a0a6542cac
Blackie360 <felixkent360@gmail.com>
2024-01-26 14:14:27 +0300

    view

---
+++ app/f/[fileId]/component/FileItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FileItem = ({file}) => {
+  return (
+    <div>FileItem</div>
+  )
+}
+
+export default FileItem
--- app/f/[fileId]/page.js
+++ app/f/[fileId]/page.js
@@ -1,15 +1,34 @@
 "use client"  
-import React, {useEffect} from "react"
+import {app} from "@/firebaseconfig"
+import { doc, getDoc, getFirestore } from "firebase/firestore"
+import React, {useEffect, useState} from "react"
+import FileItem from "./component/FileItem"
 
 
 
 const FileView = ({params}) => {
+    const db =getFirestore(app);
+    const [file, setFile] = useState();
    
     useEffect(()=>{
-
+        params?.fileId && getFileInfo();
     },[])
+    const getFileInfo=async()=>{
+        const docRef = doc(db, "uploadedFile", params?.fileId);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          console.log("Document data:", docSnap.data());
+          setFile(docsnap.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+            
+        }
+    }
   return (
-    <div>FileView</div>
+    <div className="bg-slate-100 h-screen w-full flex justify-center items-center flex-col gap-4">
+        <FileItem file={file}/>
+    </div>
   )
 }
 
