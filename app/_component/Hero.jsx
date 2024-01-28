dd3fd94b837a3ec1b2a7a5f8cd354a2a0fd6e9cc
Blackie360 <felixkent360@gmail.com>
2024-01-28 13:46:46 +0300

    nav

--- app/_component/Hero.jsx
+++ app/_component/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
 
             <div className="mt-8 flex flex-wrap justify-center gap-4">
               <a
-                className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-black hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-black hover:bg-transparent hover:text-black focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
                 href="/files"
               >
                 Get Started
